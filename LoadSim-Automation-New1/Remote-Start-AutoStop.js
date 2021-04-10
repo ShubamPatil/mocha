539f86038609e1a78c81f68a7f72e06996f66ad1
ShubamPatil <shubam.patil@gmail.com>
2021-04-10 21:00:10 +0530

    10-04-2021

--- LoadSim-Automation-New1/Remote-Start-AutoStop.js
+++ LoadSim-Automation-New1/Remote-Start-AutoStop.js
@@ -7,38 +7,35 @@ remoteStop = require("./remote-start-stop/remote-stop").triggerRemoteStop;
 
 let flag = true;
 var transID;
-const startDatetime = new Date();
+// const startDatetime = new Date();
 
 describe('-----Test Case - 021-----', function () {
     it('-----Remote-Start-AutoStop------', function (done) {
-        startSimulator();
 
-        function startSimulator() {
-            const child = spawn('bash', ['TC-021-AutoStopForRemoteStart/start-simulator.sh']);
+        const child = spawn('bash', ['TC-021-AutoStopForRemoteStart/start-simulator.sh']);
 
-            child.stdout.on('data', (chunk) => {
-                chunk = chunk.toString();
-                console.log(`sim says: ${chunk}`);
+        child.stdout.on('data', (chunk) => {
+            chunk = chunk.toString();
+            console.log(`sim says: ${chunk}`);
 
-                if (chunk.includes("Waiting for Remote Start")) {
-                    remoteStart();
-                }
+            if (chunk.includes("Waiting for Remote Start")) {
+                remoteStart();
+            }
 
-                if (chunk.includes("transactionId") && flag === true) {
-                    transID = chunk.split("transactionId: ")[1].split(" }")[0];
-                    flag = false;
-                    console.log(transID);
-                }
+            if (chunk.includes("transactionId") && flag === true) {
+                transID = chunk.split("transactionId: ")[1].split(" }")[0];
+                flag = false;
+                console.log(transID);
+            }
 
-                if (chunk.includes("Stopped charging")) {
-                    const endDatetime = new Date();
-                    CheckInvoice(startDatetime, endDatetime, transID);
-                    done();
-                }
+            if (chunk.includes("Stopped charging")) {
+                const endDatetime = new Date();
+                CheckInvoice(transID);
+                done();
+            }
 
-            });
+        });
 
-        }
 
     })
 })
--- LoadSim-Automation-New1/check-invoice.js
+++ LoadSim-Automation-New1/check-invoice.js
@@ -1,35 +1,32 @@
 const chai = require('chai');
 chai.use(require('chai-http'));
 const { expect } = require("chai");
+const d = new Date();
+let a = d.toDateString(), parts = a.split(" ");
+const date = parts[2], day = parts[0], month = parts[1], year = parts[3];
 
 
-
-module.exports.checkInvoiceGenerated = function (startDatetime, endDatetime, transID) {
+module.exports.checkInvoiceGenerated = function (transID) {
     describe('checking-invoice-for-current-time-after-the-self-remote-stop', () => {
-        it('check invoice', (done) => {
-            // setTimeout(() => {
+        it('check invoice', () => {
                 return chai.request('https://testcms1.numocity.com:4003/')
-                    .get('transaction/get/v1?fromDate=Sun%20Apr%2004%202021%2001:01:01%20GMT+0530%20(India%20Standard%20Time)&toDate=Sun%20Apr%2004%202021%2023:59:59%20GMT+0530%20(India%20Standard%20Time)')
+                    .get(`transaction/get/v1?fromDate=${day}%20${month}%20${date}%20${year}%2001:01:01%20GMT+0530%20(India%20Standard%20Time)&toDate=${day}%20${month}%20${date}%20${year}%2023:59:59%20GMT+0530%20(India%20Standard%20Time)`)
                     .set('content-type', 'application/json')
-                    .end((err, res) => {
-                        expect(err).to.be.null;
+                    .then((res) => {
                         expect(res).to.have.status(200);
                         console.log(`got ${res.body.Document.length} total sessions`);
                         const todaysSession = res.body.Document.filter((session) => {
-                            endDatetime = new Date();
-                            return endDatetime > startDatetime && session.CPID.startsWith('Testcharge001');
+                            return session.CPID.startsWith('Testcharge001');
                         });
                         console.log(`${todaysSession.length} of them are for Testcharge001`);
-                        const ActTransID = todaysSession[0].TransactionID;
-
+                        const ActTransID = (todaysSession[0].TransactionID).toString();
+                    
                         console.log(transID);
                         console.log(`Recent Transaction ID : ${ActTransID}`);
-                        // expect(ActTransID).to.be.equal(transID);
+                        expect(ActTransID).to.be.equal(transID);
                         
                         //spawn('bash', ['stop-simulator.sh']);
-                        done();
                     })
-            // }, 100000);
         })
     })
 
